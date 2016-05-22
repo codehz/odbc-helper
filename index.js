10efeca92fd7f432fb93a55e58eda3fed6336431
CodeHz <CodeHz@Outlook.com>
2016-05-22 13:52:08 +0800

    fix bugs and add trigger support

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const ODBC = require('odbc');
 
-const checkProperty = property => typeof property === 'string' && property.startsWith('$');
+const checkProperty = (property, ch = '$') => typeof property === 'string' && property.startsWith(ch);
 const callback2promise = (func, ...args) =>
     new Promise((resolve, reject) => func(...args, (err, ret) => err ? reject(err) : resolve(ret)));
 const genConstruct = construct => new Proxy(function () {}, {
     construct
 });
-const log = content => (DB.debug && console.log(content), content);
-const mixFunction = mix => (func => (Object.keys(mix).forEach(key => func[key] = mix[key]), func))(function () {});
-const mapParams = (maps, txt) => txt.replace(/\$\w+/gi, match => (maps.push(match), '?'));
+const log = (...content) => (DB.debug && console.log(...content), content.pop());
+const fakeFunction = mix => (func => (Object.keys(mix).forEach(key => func[key] = mix[key]), func))(function () {});
+const mapParams = (maps, txt) => (maps.splice(0),
+    txt.replace(/\$\w+/gi, match => ((maps.includes(match) || maps.push(match)), '?')));
 const unmapParams = (maps, value) => (arr =>
     (Object.keys(value).forEach(v =>
         arr[maps.indexOf(v)] = value[v]), arr))([]);
+const directMapParams = (map, txt) => txt.replace(/\$\w+/gi, match => map[match]);
 
 const DBMethods = (db, table) => ({
     create: genConstruct(() => {
-        const PRIMARY = keys => keys ? `PRIMARY KEY(${keys.join(', ')})` : '';
-        const generator = target => target.map(item => [item.name, item.type]
-                .concat(item.unique ? 'UNIQUE' : [])
-                .concat(item.notnull ? 'NOT NULL' : [])
-                .concat(item.default ? `DEFAULT ${item.default}` : [])
-                .join(' '))
-            .concat(PRIMARY(target.filter(item => item.conf.primary).map(item => item.name)))
-            .concat(target.filter(item => item.conf.foreign)
+        const PRIMARY = keys => keys ? [
+            ['PRIMARY KEY', `(${keys.join(', ')})`].join(' ')
+        ] : [];
+        const generator = target => [
+                ...target.map(item => [
+                        item.name,
+                        item.type,
+                        ...(item.conf.unique ? ['UNIQUE'] : []),
+                        ...(item.conf.notnull ? ['NOT NULL'] : []),
+                        ...(item.conf.default ? ['DEFAULT', item.conf.default] : []),
+                    ]
+                    .join(' ')),
+                ...PRIMARY(target.filter(item => item.conf.primary).map(item => item.name)),
+                ...target.filter(item => item.conf.foreign)
                 .map(({
                     name: src,
                     conf: {
@@ -31,10 +39,12 @@ const DBMethods = (db, table) => ({
                             key = src
                         }
                     }
-                }) => `FOREIGN KEY(${src}) REFERENCES ${target}(${key}) ON DELETE CASCADE`))
+                }) => `FOREIGN KEY(${src}) REFERENCES ${target}(${key}) ON DELETE CASCADE`)
+            ]
             .join(', ');
-        const self = new Proxy(mixFunction({
-            content: []
+        const self = new Proxy(fakeFunction({
+            content: [],
+            maps: []
         }), {
             get: (target, property) => checkProperty(property) ?
                 (name =>
@@ -42,24 +52,35 @@ const DBMethods = (db, table) => ({
                         name,
                         type,
                         conf
-                    }), self))(property.substr(1)) : target[property],
-            apply: target => (stmt => callback2promise(stmt.execute.bind(stmt)))
-                (db.prepareSync(log(`CREATE TABLE IF NOT EXISTS ${table} (${generator(target.content)})`)))
+                    }), self))(property.substr(1)) : property === 'raw' ?
+                mapParams(target.maps, [
+                    'CREATE TABLE IF NOT EXISTS',
+                    table,
+                    `(${generator(target.content)})`
+                ].join(' ')) : target[property],
+            apply: (target, ctx, [value = {}]) => (stmt =>
+                    callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
+                (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
         });
         return self;
     }),
     insert: genConstruct(() => {
-        const self = new Proxy(mixFunction({
+        const self = new Proxy(fakeFunction({
             keys: [],
             maps: [],
             stmt: null
         }), {
             get: (target, property) => checkProperty(property) ?
-                (target.keys.push(property.substr(1)), self) : target[property],
+                (target.keys.push(property.substr(1)), self) : property === 'raw' ? [
+                    'INSERT INTO',
+                    table,
+                    `(${target.keys.join(', ')})`,
+                    'VALUES',
+                    `(${target.keys.map(key => '$' + key).join(', ')})`
+                ].join(' ') : target[property],
             apply: (target, ctx, [value = {}]) => (stmt =>
                     callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
-                (target.stmt || (target.stmt = db.prepareSync(log(mapParams(target.maps,
-                    `INSERT INTO ${table} (${target.keys.join(', ')}) VALUES (${target.keys.map(key => '$' + key).join(', ')})`))))),
+                (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
         });
         return log(self);
     }),
@@ -69,7 +90,7 @@ const DBMethods = (db, table) => ({
                 target.data ? rv(target.data) :
                 target.fetchAll((err, data) => (target.closeSync(), err ? rj(err) : rv(data)))) : target[property]
         }))).catch(reject));
-        const self = new Proxy(mixFunction({
+        const self = new Proxy(fakeFunction({
             stmt: null,
             keys: [],
             maps: [],
@@ -85,23 +106,26 @@ const DBMethods = (db, table) => ({
                 cond => (target.wheres.push(cond), self) : property === 'order' ?
                 (by, asc = 'asc') => (target.orders.push({
                     by, asc
-                }), self) : target[property],
+                }), self) : property === 'raw' ? ['SELECT',
+                    target.keys.join(', '),
+                    'FROM',
+                    table,
+                    ...(target.wheres.length > 0 ? ['WHERE', target.wheres.join(' AND ')] : []),
+                    ...(target.orders.length > 0 ? ['ORDER BY', target.orders.map(({
+                        by, asc
+                    }) => `${by} ${asc}`).join(', ')] : []),
+                    ...(target.limit.offset ? ['LIMIT', `${target.limit.offset}, ${target.limit.rows}`] : [])
+                ]
+                .join(' ') : target[property],
             apply: (target, ctx, [value = {}]) => (stmt =>
                     resultHelper(callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value))))
-                (target.stmt || (target.stmt =
-                    db.prepareSync(log(mapParams(target.maps, [`SELECT ${target.keys.join(', ')} FROM ${table}`]
-                        .concat(target.wheres.length > 0 ? `WHERE ` + target.wheres.join(' AND ') : [])
-                        .concat(target.orders.length > 0 ? `ORDER BY ` + target.orders.map(({
-                            by, asc
-                        }) => `${by} ${asc}`).join(', ') : [])
-                        .concat(target.limit.offset ? `LIMIT ${target.limit.offset}, ${target.limit.rows}` : [])
-                        .join(' '))))))
+                (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
         });
         return self;
     }),
     update: genConstruct(() => {
         const generator = keys => keys.map(key => `${key} = \$${key}`).join(', ');
-        const self = new Proxy(mixFunction({
+        const self = new Proxy(fakeFunction({
             keys: [],
             wheres: [],
             maps: [],
@@ -109,38 +133,87 @@ const DBMethods = (db, table) => ({
         }), {
             get: (target, property) => checkProperty(property) ?
                 (target.keys.push(property.substr(1)), self) : property === 'where' ?
-                cond => (target.wheres.push(cond), self) : target[property],
+                cond => (target.wheres.push(cond), self) : property === 'raw' ? [`UPDATE ${table} SET ${generator(target.keys, target.maps)}`]
+                .concat(target.wheres.length > 0 ? `WHERE ` + target.wheres.join(' AND ') : [])
+                .join(' ') : target[property],
             apply: (target, ctx, [value = {}]) => (stmt =>
                     callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
-                (target.stmt || (target.stmt =
-                    db.prepareSync(log(mapParams(
-                        target.maps, [`UPDATE ${table} SET ${generator(target.keys, target.maps)}`]
-                        .concat(target.wheres.length > 0 ? `WHERE ` + target.wheres.join(' AND ') : [])
-                        .join(' ')))))),
+                (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
         });
         return self;
     }),
     delete: genConstruct(() => {
-        const self = new Proxy(mixFunction({
+        const self = new Proxy(fakeFunction({
             stmt: null,
             wheres: [],
             maps: [],
         }), {
             get: (target, property) => property === 'where' ?
-                cond => (target.wheres.push(cond), self) : target[property],
+                cond => (target.wheres.push(cond), self) : property === 'raw' ? [`DELETE FROM ${table}`]
+                .concat(target.wheres.length > 0 ? `WHERE ${target.wheres.join(' AND ')}` : [])
+                .join(' ') : target[property],
             apply: (target, ctx, [value = {}]) => (stmt =>
                     callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
-                (target.stmt || (target.stmt = db.prepareSync(log(mapParams(target.maps, [`DELETE FROM ${table}`]
-                    .concat(target.wheres.length > 0 ? `WHERE ` + target.wheres.join(' AND ') : [])
-                    .join(' '))))))
+                (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
         });
         return self;
     })
 });
 
+const others = {
+    raw: db => (text) => new Proxy(fakeFunction({
+        data: text,
+        stmt: null,
+        maps: []
+    }), {
+        get: (target, property) => property === 'raw' ?
+            text : target[property],
+        apply: (target, ctx, [value = {}]) => (stmt =>
+                callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
+            (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, text))))),
+    }),
+    trigger: db => new Proxy(db, {
+        get: (clazz, trigger_name) => checkProperty(trigger_name) ? genConstruct(() => {
+            const self = new Proxy(fakeFunction({
+                after: true,
+                event: '',
+                on: '',
+                ops: [],
+                maps: []
+            }), {
+                get: (obj, property) => property === 'before' ?
+                    ((obj.after = false), event => ((obj.event = event), self)) : property === 'after' ?
+                    ((obj.after = true), event => ((obj.event = event), self)) : property === 'on' ?
+                    (on => ((obj.on = on), self)) : property === 'do' ?
+                    ((op, args) => (obj.ops.push((args ? directMapParams(args, op.raw) : op.raw) + ';'), self)) : property === 'raw' ? [
+                        'CREATE TRIGGER IF NOT EXISTS',
+                        trigger_name.substr(1),
+                        obj.after ? 'AFTER' : 'BEFORE',
+                        obj.event,
+                        'ON',
+                        obj.on,
+                        'FOR EACH ROW BEGIN',
+                        ...obj.ops,
+                        'END'
+                    ].join(' ') : obj[property],
+                apply: (target, ctx, [value = {}]) => (stmt =>
+                        callback2promise(stmt.execute.bind(stmt), unmapParams(target.maps, value)))
+                    (target.stmt || (target.stmt = db.prepareSync(log('create', mapParams(target.maps, self.raw))))),
+            });
+            return self;
+        }) : clazz[trigger_name],
+        deleteProperty: (target, property) => checkProperty(property) ?
+            (others.raw(db)(`DROP TRIGGER IF EXISTS ${property.substr(1)}`)(), true) : false
+    })
+};
+
 function setup(db) {
     return new Proxy(db, {
-        get: (target, property) => checkProperty(property) ? DBMethods(target, property.substr(1)) : target[property]
+        get: (target, property) => checkProperty(property, '$') ?
+            DBMethods(target, property.substr(1)) : checkProperty(property, '_') ?
+            others[property.substr(1)](db) : target[property],
+        deleteProperty: (target, property) => checkProperty(property) ?
+            (others.raw(db)(`DROP TABLE IF EXISTS ${property.substr(1)}`)(), true) : false
     });
 }
 
--- test.js
+++ test.js
@@ -4,7 +4,21 @@ console.log('start');
 helper.debug = true;
 
 helper('DRIVER=SQLite3;Database=./test.db;FKSupport=True').then(async function (db) {
-        const createTable = new db.$test.create()
+        const logTable = new db.$log.create()
+            .$id('INTEGER', {
+                primary: true,
+                unique: true
+            })
+            .$src('TINYTEXT', {
+                notnull: true
+            })
+            .$content('TEXT', {
+                notnull: 'true'
+            })
+            .$ctime('DATETIME', {
+                default: 'CURRENT_TIMESTAMP'
+            });
+        const testTable = new db.$test.create()
             .$id('INTEGER', {
                 primary: true,
                 unique: true
@@ -13,9 +27,21 @@ helper('DRIVER=SQLite3;Database=./test.db;FKSupport=True').then(async function (
                 default: 'test'
             })
             .$info('TEXT');
+
         console.log('create', typeof createTable);
-        await createTable();
+        await logTable();
+        await testTable();
         console.log('create', 'after create');
+
+        const testLogTrigger = new db._trigger.$testlog()
+            .after('INSERT').on('test').do(new db.$log.insert().$src.$content, {
+                $src: '"test"',
+                $content: '"insert " || NEW.id'
+            });
+        console.log(testLogTrigger.do);
+        await testLogTrigger();
+
+        const selectLogData = new db.$log.select().$;
 
         const insertData = new db.$test.insert().$name.$info;
         console.log('insert');
@@ -37,20 +63,27 @@ helper('DRIVER=SQLite3;Database=./test.db;FKSupport=True').then(async function (
         });
         console.log('insert', 'after insert');
 
+        console.log(await (await selectLogData()).data);
+
         const selectData = new db.$test.select().$;
         console.log(await (await selectData()).data);
         console.log('select', 'after select');
 
         console.log('delete');
         const deleteCommand = new db.$test.delete().where('name = $name');
-        await deleteCommand({$name: 'TEST'});
+        await deleteCommand({
+            $name: 'TEST'
+        });
         console.log('delete', 'after delete');
 
         console.log(await (await selectData()).data);
 
         console.log('update');
         const updateCommand = new db.$test.update().$info.where('name == $name');
-        await updateCommand({$name: 'TEST2', $info: 'modify'});
+        await updateCommand({
+            $name: 'TEST2',
+            $info: 'modify'
+        });
         console.log('update', 'after update');
 
         console.log(await (await selectData()).data);
@@ -81,5 +114,10 @@ helper('DRIVER=SQLite3;Database=./test.db;FKSupport=True').then(async function (
         await new db.$test.delete()();
 
         console.log(await (await selectAnotherData()).data);
+
+        delete db._trigger.$testlog;
+        delete db.$log;
+        delete db.$test;
+        delete db.$another;
     })
     .catch(err => console.error('err', err));
